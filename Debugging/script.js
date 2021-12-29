019c73d2940d9f6ca9beec0f9ce24cf8e417b42e
ec-dani <jd.cantuna@alumnos.upm.es>
2021-12-29 22:29:49 +0100

    solucion html

---
+++ Debugging/script.js
@@ -0,0 +1,4 @@
+function showMenu(elmnt) { document.getElementById(elmnt).style.visibility="visible"; }
+function hideMenu(elmnt) { document.getElementById(elmnt).style.visibility="hidden"; }
+function fin(){alert('Fin de la Aplicacion'); window.close();}
+function ir(nombre){ document.location=nombre+".html" }
